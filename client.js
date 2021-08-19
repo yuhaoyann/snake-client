eff8b05bc921d35692e526cdbc086f28e9873bc8
Haoyan Yu <haoyan@HaoyandeMacBook-Pro.local>
2021-08-18 23:49:51 -0400

    created client file for connecting to server

---
+++ client.js
@@ -0,0 +1,27 @@
+const net = require('net');
+
+const connect = function() {
+
+  const conn = net.createConnection({
+    host: '192.168.1.104',
+    port: 50541
+  });
+
+  conn.setEncoding('utf8');
+
+  conn.on('connect', () => {
+    console.log('Successfully connected to game server');
+    conn.write('Name: YHY');
+    // setInterval(() => conn.write('Move: up'), 50);
+  })
+
+  conn.on('data', (data) => {
+    console.log(data);
+  })
+
+  return conn;
+}
+
+module.exports = {
+  connect
+};
